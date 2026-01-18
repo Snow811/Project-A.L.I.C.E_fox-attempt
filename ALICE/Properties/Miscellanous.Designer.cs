7c7491954cd23e57ae822e58e314151a3b50ca79
snowf <snowf@FOXBOX>
2026-01-18 16:14:21 -0500

    updatestuff

--- ALICE/Properties/Miscellanous.Designer.cs
+++ ALICE/Properties/Miscellanous.Designer.cs
@@ -12,7 +12,7 @@ namespace ALICE.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Miscellanous : global::System.Configuration.ApplicationSettingsBase {
         
         private static Miscellanous defaultInstance = ((Miscellanous)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Miscellanous())));
